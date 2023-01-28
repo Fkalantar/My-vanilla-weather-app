4feea55c28896ede02980146601a7dceb61488d8
Fkalantar <122690614+Fkalantar@users.noreply.github.com>
2023-01-28 15:20:56 +0330

    Updated API Key

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function displayForecast(response) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        ` <div class="col-2">
+        `<div class="col-2">
                 <div class="weather-forecast-date">${formatDay(
                   forecastDay.dt
                 )}</div>
@@ -47,14 +47,14 @@ function displayForecast(response) {
                   src="http://openweathermap.org/img/wn/${
                     forecastDay.weather[0].icon
                   }@2x.png"
-                  alt=""
+                  alt="#"
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max">${Math.round(
+                  <span class="weather-forecast-temperatures-max">${Math.round(
                     forecastDay.temp.max
                   )}° </span>
-                  <span class="weather-forecast-temperature-min">${Math.round(
+                  <span class="weather-forecast-temperatures-min">${Math.round(
                     forecastDay.temp.min
                   )}° </span>
                 </div>
@@ -67,9 +67,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "96f633068ece54dc03b4a1ba9571d807";
+  let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -82,7 +82,7 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.temp;
+  let celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
